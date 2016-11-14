10c161602973a34ea59696fbe232a145ec08dec6
dohertki <dohertki@oregonstate.edu>
2016-11-14 11:54:55 -0800

    Start server

---
+++ ftserver.c
@@ -0,0 +1,47 @@
+
+
+/***************************************************************
+ * Kierin Doherty (dohertki)
+ *
+ * ftserver.c: 
+ *
+ * input: $ ftserver <Port #> 
+ *
+ * output: Server unencrypts message passed to it by the 
+ *         the key passed via same socket and returns 
+ *         a plaintext message
+ *
+ **************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
+
+
+
+
+int main(int argc, char *argv[]){
+
+
+
+
+
+
+
+
+
+
+    return 0;    
+}
+
+
